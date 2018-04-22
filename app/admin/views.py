3e687077fce8755c5c8b236fc963ae7627e01eb6
stav <stav@stavs-MacBook-Pro.local>
2018-04-22 22:52:23 +0800

    18.04.22

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,21 @@
 # coding:utf8
+from flask import render_template, redirect, url_for
+
 from . import admin
 
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>this is admin</h1>"
+    return render_template("admin/index.html")
+
+
+
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
